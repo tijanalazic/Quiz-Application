323ab0963162d550b1f28dcb861f6336ed8ce26d
TijanaLazic <tijanalazic0000@gmail.com>
2023-10-08 21:14:59 +0200

    Quiz Application - Question Model and Creation of Questions

---
+++ app/src/main/java/com/example/quizapplication/Constants.kt
@@ -0,0 +1,141 @@
+package com.example.quizapplication
+
+object Constants {
+
+    fun getQuestions() : ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+        val question1 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+
+        questionsList.add(question1)
+
+        val question2 = Question(
+            2,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+
+            "Angola",
+            "Austria",
+            "Australia",
+            "Armenia",
+            3
+        )
+
+        questionsList.add(question2)
+
+        val question3 = Question(
+            3,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Belarus",
+            "Belize",
+            "Brunei",
+            "Brazil",
+            4
+        )
+
+        questionsList.add(question3)
+
+        val question4 = Question(
+            4,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bahamas",
+            "Belgium",
+            "Barbados",
+            "Belize",
+            2
+        )
+
+        questionsList.add(question4)
+
+        val question5 = Question(
+            5,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Gabon",
+            "France",
+            "Fiji",
+            "Finland",
+            3
+        )
+
+        questionsList.add(question5)
+
+        val question6 = Question(
+            6,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Germany",
+            "Georgia",
+            "Greece",
+            "none of these",
+            1
+        )
+
+        questionsList.add(question6)
+
+        val question7 = Question(
+            7,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Dominica",
+            "Egypt",
+            "Denmark",
+            "Ethiopia",
+            3
+        )
+
+        questionsList.add(question7)
+
+        val question8 = Question(
+            8,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Ireland",
+            "Iran",
+            "Hungary",
+            "India",
+            4
+        )
+
+        questionsList.add(question8)
+
+        val question9 = Question(
+            9,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia",
+            "New Zealand",
+            "Tuvalu",
+            "United States of America",
+            2
+        )
+
+        questionsList.add(question9)
+
+        val question10 = Question(
+            10,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Kuwait",
+            "Jordan",
+            "Sudan",
+            "Palestine",
+            1
+        )
+
+        questionsList.add(question10)
+
+        return questionsList
+    }
+}
--- app/src/main/java/com/example/quizapplication/MainActivity.kt
+++ app/src/main/java/com/example/quizapplication/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.quizapplication
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.quizapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var button : Button
+    //private lateinit var name: EditText
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //button = findViewById(R.id.btnStart)
+        //name = findViewById(R.id.et_name)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        binding.btnStart.setOnClickListener {
+            if(binding.etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
